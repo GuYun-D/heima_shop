b83a25de279dbcfc3b521a2bb626ea1ed3988ac5
GuYun-D <1946306647@qq.com>
2021-03-23 09:35:12 +0800

    请求轮播图数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,65 +1,21 @@
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    
+    // 轮播图数据数组
+    swiperList: []
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  // 页面开始加载时触发
+  onLoad(){
+    // 发送异步请求
+    wx.request({
+      // 请求路径
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+      success: (result) => {
+        console.log(result);
+        this.setData({
+          swiperList: result.data.message
+        })
+      }
+    })
   }
 })
