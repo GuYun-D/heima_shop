334d7b3ee3d7e6dc5c2dfb828f73428e59dd20fd
GuYun-D <1946306647@qq.com>
2021-03-25 17:52:53 +0800

    下拉加载数据

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,3 +1,22 @@
+/**
+ *  用户上滑页面 滚动条触底 开始加载下一页数据
+ *  1 找到滚动条触底事件  微信小程序官方开发文档寻找
+ *  2 判断还有没有下一页数据
+ *    1 获取到总页数  只有总条数
+ *      总页数 = Math.ceil(总条数 /  页容量  pagesize)
+ *      总页数     = Math.ceil( 23 / 10 ) = 3
+ *    2 获取到当前的页码  pagenum
+ *    3 判断一下 当前的页码是否大于等于 总页数 
+ *      表示 没有下一页数据
+ * 
+ *  3 假如没有下一页数据 弹出一个提示
+ *  4 假如还有下一页数据 来加载下一页数据
+ *    1 当前的页码 ++
+ *    2 重新发送请求
+ *    3 数据请求回来  要对data中的数组 进行 拼接 而不是全部替换！！！
+ *    
+ */
+
 import {
   request
 } from "../../request/index.js"
@@ -24,7 +43,7 @@ Page({
     }],
 
     // 获取到的商品列表数据
-    goodsList: []
+    goodsList: [],
   },
 
   // 请求商品数据的参数
@@ -32,10 +51,13 @@ Page({
     query: "",
     cid: "",
     // 请求第几页
-    pagenum: "",
+    pagenum: 1,
     // 页容量
-    pagesize: ""
+    pagesize: 10
   },
+
+  // 获取到的总页数
+  totalPages: 1,
 
   /**
    * 生命周期函数--监听页面加载
@@ -74,8 +96,38 @@ Page({
     })
     // console.log(res);
 
+    // 获取 总条数
+    const total = res.total;
+    // 计算总页数
+    this.totalPages = Math.ceil(total / this.QueryParams.pagesize);
+    // console.log("pagenum=" + this.QueryParams.pagenum);
+    // console.log("totalpages=" + this.totalPages);
     this.setData({
-      goodsList: res.goods
+      // 拼接数组，当
+      // goodsList: res.goods
+      // 当下拉请求回来的数据只有下一页的数据，就需要进行拼接
+      goodsList: [...this.data.goodsList, ...res.goods]
     })
+  },
+
+  // 页面上滑、滚动条触底事件
+  onReachBottom() {
+    // console.log("页面触底");
+    // 判断还有没有数据
+    if (this.QueryParams.pagenum >= this.totalPages) {
+      // 没有下一页数据
+      //  console.log('%c'+"没有下一页数据","color:red;font-size:100px;background-image:linear-gradient(to right,#0094ff,pink)");
+      // 提示框
+      wx.showToast({
+        title: '没有下一页数据'
+      });
+
+    } else {
+      // 还有下一页数据
+      //  console.log('%c'+"有下一页数据","color:red;font-size:100px;background-image:linear-gradient(to right,#0094ff,pink)");
+      this.QueryParams.pagenum++;
+      this.getGoodsList();
+    }
   }
+
 })
