20afee799c77f4819180cb4af71d51bdb2c03c57
GuYun-D <1946306647@qq.com>
2021-03-27 13:17:30 +0800

    获取收货地址

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -15,52 +15,16 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // d点击收货地址触发
+  handleChooseAdress(){
+    // console.log("选择收货地址");
+    // 获取收货地址
+    wx.chooseAddress({
+      success: (address) => {
+        // console.log(address);
+        // 将收货地址放到缓存中去
+        wx.setStorageSync('address', address);
+      }
+    })
   }
 })
