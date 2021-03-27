93d79b64581d28d099ad9bbbbce3881d5935cfb8
GuYun-D <1946306647@qq.com>
2021-03-27 11:48:57 +0800

    加入购物车功能

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -2,6 +2,13 @@
  * 点击图片预览图片
  *      1.给轮播图绑定点击事件
  *      2.调用小程序的api previewImage
+ * 点击加入购物车
+ *      1 先绑定点击事件
+ *      2 获取缓存中的购物车数据 数组格式 
+ *      3 先判断 当前的商品是否已经存在于 购物车
+ *      4 已经存在 修改商品数据  执行购物车数量++ 重新把购物车数组 填充回缓存中
+ *      5 不存在于购物车的数组中 直接给购物车数组添加一个新元素 新元素 带上 购买数量属性 num  重新把购物车数组 填充回 缓存中
+ *      6 弹出提示
  */
 
 import {
@@ -68,5 +75,32 @@ Page({
       current,
       urls
     });
+  },
+
+  // 点击加入购物车
+  handleCartAdd() {
+    // console.log("加入购物车");
+    //  获取缓存中的购物车数据 数组格式 
+    let cart = wx.getStorageSync('cart') || [];
+    // 先判断 当前的商品是否已经存在于 购物车
+    let index = cart.findIndex(v => v.goods_id === this.GoodsInfo.goods_id);
+    if (index === -1) {
+      // 不存在，第一次添加
+      this.GoodsInfo.num = 1;
+      cart.push(this.GoodsInfo);
+    } else {
+      // 已经存在，执行num++
+      cart[index].num++;
+    }
+    // 重新把购物车数组 填充回缓存中
+    wx.setStorageSync('cart', cart);
+    // 弹窗提示
+    wx.showToast({
+      title: '加入成功',
+      icon: "success",
+      // 遮罩，防止手抖，多次添加
+      mask: true
+    })
+
   }
 })
