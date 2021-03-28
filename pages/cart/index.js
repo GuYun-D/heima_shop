c70ca941268edf7054a9eec94446d74746de2e48
GuYun-D <1946306647@qq.com>
2021-03-28 11:49:35 +0800

    商品选中

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -10,12 +10,18 @@
  *    根据购物车中的商品数据 所有的商品都被选中 checked=true  全选就被选中
  * 总价格和总数量
  *    都需要商品被选中 我们才拿它来计算
- *   获取购物车数组
- *   遍历
- *   判断商品是否被选中
- *   总价格 += 商品的单价 * 商品的数量
- *   总数量 +=商品的数量
- *   把计算后的价格和数量 设置回data中即可
+ *    获取购物车数组
+ *    遍历
+ *    判断商品是否被选中
+ *    总价格 += 商品的单价 * 商品的数量
+ *    总数量 +=商品的数量
+ *    把计算后的价格和数量 设置回data中即可
+ * 商品选中
+ *      绑定change事件
+ *      获取到被修改的商品对象
+ *      商品对象的选中状态 取反
+ *      重新填充回data中和缓存中
+ *      重新计算全选。总价格 总数量。。。
  */
 Page({
   data: {
@@ -93,6 +99,50 @@ Page({
         address.all = address.provinceName + address.cityName + address.countyName + address.detailInfo;
         wx.setStorageSync('address', address);
       }
+    })
+  },
+
+  // 商品的选中
+  handleItemChange(e) {
+    // 获取被修改的商品的id
+    const goodsId = e.currentTarget.dataset.id;
+    // 获取购物车数组
+    let {
+      cart
+    } = this.data;
+    console.log(cart);
+    // 找到被修改的商品对象
+    let index = cart.findIndex(v => v.goods_id === goodsId);
+    console.log(index);
+    // 选中状态取反
+    cart[index].checked = !cart[index].checked;
+    // 将购物车数据重新设置回data中和缓存中
+
+
+    wx.setStorageSync('cart', cart)
+
+    let allChecked = true;
+    // 总价&总数
+    let totalPrice = 0;
+    let totalNum = 0;
+    cart.forEach(v => {
+      if (v.checked) {
+        totalPrice += v.num * v.goods_price;
+        totalNum += v.num;
+      } else {
+        /**
+         * 全选优化
+         * 通过计算总价的前提就是所有商品选中，如果有一个商品未选中就走此路，将赋值为false
+         * 另外判断数组是否为空
+         */
+        allChecked = false
+      }
+    });
+    // 判断cart是否为空
+    allChecked = cart.length != 0 ? allChecked : false
+
+    this.setData({
+      cart,totalNum,totalPrice,allChecked
     })
   }
 })
