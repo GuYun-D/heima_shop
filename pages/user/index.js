4ee4fd751e82068e8c6dbfc0de3d569995e0a574
GuYun-D <1946306647@qq.com>
2021-03-29 19:56:48 +0800

    显示收藏的商品数量

--- pages/user/index.js
+++ pages/user/index.js
@@ -1,13 +1,22 @@
 Page({
   data: {
-    userInfo: {}
+    userInfo: {},
+    // 被收藏的商品的数量
+    collectNum: 0
   },
 
   onShow() {
     // 获取用户信息
     const userInfo = wx.getStorageSync('userinfo');
+    // this.setData({
+    //   userInfo
+    // })
+
+    // 获取缓存中的收藏
+    const collect = wx.getStorageSync('collect') || [];
     this.setData({
-      userInfo
+      userInfo,
+      collectNum: collect.length
     })
   }
 })
