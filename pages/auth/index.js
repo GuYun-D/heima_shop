1f7281f8de3e8c680a49d7c02be5c1f06e5c248d
GuYun-D <1946306647@qq.com>
2021-03-29 16:24:51 +0800

    模拟token

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -45,15 +45,17 @@ Page({
         code
       };
       //  3 发送请求 获取用户的token
-      const {
-        token
-      } = await request({
-        url: "/users/wxlogin",
-        data: loginParams,
-        method: "post"
-      });
+      // const {
+      //   token
+      // } = await request({
+      //   url: "/users/wxlogin",
+      //   data: loginParams,
+      //   method: "post"
+      // });
       // 4 把token存入缓存中 同时跳转回上一个页面
-      wx.setStorageSync("token", token);
+      // wx.setStorageSync("token", token);
+      // monitoken
+      wx.setStorageSync('token', "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjIzLCJpYXQiOjE1NjQ3MzAwNzksImV4cCI6MTAwMTU2NDczMDA3OH0.YPt-XeLnjV-_1ITaXGY2FhxmCe4NvXuRnRB8OMCfnPo appid: wxfb52f2d7b2f6123a")
       wx.navigateBack({
         delta: 1
       });
